57e18b04b4f7f457bb3d4c2632dc0046eb40b894
James Moriarty <james.moriarty@rea-group.com>
2017-06-08 20:59:52 +1000

    refinement: move side effect to edge.

--- src/main/scala/example/ToyRobot.scala
+++ src/main/scala/example/ToyRobot.scala
@@ -3,13 +3,13 @@ package example
 import scala.util.{Failure, Success, Try}
 
 object ToyRobot extends App {
-  def exec(line: String, robot: Option[Robot]): Try[Robot] = (tokenize(line), robot) match {
-    case ("place" :: direction :: x :: y :: Nil, _) => Try(Robot(Directions.withName(direction.capitalize), x.toInt, y.toInt))
-    case ("move" :: Nil, Some(robot)) => Success(robot.move)
-    case ("left" :: Nil, Some(robot)) => Success(robot.left)
-    case ("right" :: Nil, Some(robot)) => Success(robot.right)
-    case ("report" :: Nil, Some(robot)) => println(robot); Success(robot)
-    case _ => Failure(new Exception("Command Failed"))
+  def exec(line: String, robot: Option[Robot]): (Try[Robot], Option[String]) = (tokenize(line), robot) match {
+    case ("place" :: direction :: x :: y :: Nil, _) => (Try(Robot(Directions.withName(direction.capitalize), x.toInt, y.toInt)), None)
+    case ("move" :: Nil, Some(robot))               => (Success(robot.move), None)
+    case ("left" :: Nil, Some(robot))               => (Success(robot.left), None)
+    case ("right" :: Nil, Some(robot))              => (Success(robot.right), None)
+    case ("report" :: Nil, Some(robot))             => (Success(robot), Some(robot.toString))
+    case _                                          => (Failure(new Exception("Command Failed")), None)
   }
 
   def tokenize(line: String): List[String] = line.toLowerCase.split("( |,)+").toList
@@ -18,7 +18,9 @@ object ToyRobot extends App {
     val board: Board = new Board(5, 5)
 
     args.foldLeft(None: Option[Robot]) { (robot, line) =>
-      exec(line, robot).filter((newRobot) => board.within(newRobot.x, newRobot.y)) match {
+      val (newRobot, output) = exec(line, robot)
+      output.foreach(println)
+      newRobot.filter((newRobot) => board.within(newRobot.x, newRobot.y)) match {
         case Success(newRobot) => Some(newRobot)
         case _ => robot
       }
